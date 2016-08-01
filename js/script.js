1b5a6998a6b0c9923672b38cabdd28bd971e86aa
adishap <porwaladisha@gmail.com>
2016-08-01 18:23:01 +0530

    API integrated

--- js/script.js
+++ js/script.js
@@ -4,84 +4,36 @@ var //api base url
 	authToken = '',
 	//sid
 	sid = '',
-	//Collection of all questions
-	quizQuestions = [{" What does HTML stand for?":
-							['Hyper Text Markup Language',
-							'Home Tool Markup Language',
-							'Hyperlinks and Text Markup Language']
-					},
-					{"Who is making the Web standards?" :
-							['The World Wide Web Consortium',
-							'Mozilla',
-							'Google',
-							'Microsoft']
-					},
-					{"Choose the correct HTML element for the largest heading" :
-							['<h5>',
-							'<heading>',
-							'<h1>',
-							'<h3>']
-					},
-					{"What is the correct HTML element for inserting a line break?":
-							['<break>',
-							'<br>',
-							'<lb>']
-					},
-					{"What is the correct HTML for adding a background color?" :
-							['<body style="background-color:yellow;">',
-							'<background>yellow</background>',
-							'<body bg="yellow">']
-					},
-					{"Choose the correct HTML element to define important text" :
-							['<strong>',
-							'<b>',
-							'<important>',
-							'<i>']
-					},
-					{"Choose the correct HTML element to define emphasized text" :
-							['<i>',
-							'<em>',
-							'<italic>']
-					},
-					{"What is the correct HTML for creating a hyperlink?" :
-							['<a name="http://www.w3schools.com">W3Schools.com</a>',
-							'<a url="http://www.w3schools.com">W3Schools.com</a>',
-							'<a href="http://www.w3schools.com">W3Schools</a>',
-							'<a>http://www.w3schools.com</a>']
-					},
-					{" How can you open a link in a new tab/browser window?" :
-							['<a href="url" new>',
-							'<a href="url" target="_blank">',
-							'<a href="url" target="new">']
-					},
-					{"Which of these elements are all <table> elements?" :
-							['<table><head><tfoot>',
-							'<table><tr><td>',
-							'<thead><body><tr>',
-							'<table><tr><tt>']
-					}],
-	/* correct answers are stored in the form of index of 
-	answer on the index of question in the questionsCollection array */
-	correctAnswers = [0, 0, 2, 1, 0, 0, 1, 2, 1, 1],
+	timer,
 	/*Score of test, initially 0*/
 	score = 0;
 
 /* function to make ajax request
 arguments url, method, data and a callback function*/
 function ajaxRequest(url, method, authToken, data, callback) {
-	console.log(url, method, authToken);
 	$.ajax({
 		url: url,
-		headers: { 'Access-Control-Allow-Origin' : '*',
-		"Authorization": authToken },
+		headers: {"Authorization": authToken },
 		type: method,
 		crossDomain: true,
 		data: data,
 		success: function(response) {
-			console.log(response);
-			callback(JSON.parse(response));
+			callback(response);
 		}
-    });
+	});
+}
+
+/* function to update scoring instructions*/
+function update_scoring_instructions(positive_score, negative_score){
+	var scoring_instructions_div = $('#scoring-instructions'),
+		scoring_instructions_html = '';
+
+	scoring_instructions_html = '<ul><li>For right answer => +' + positive_score;
+	scoring_instructions_html += '</li><li>For wrong answer => -' + negative_score;
+	scoring_instructions_html += '</li><li>Skip answer => 0.0</li><ul>';
+
+	//appending html to div
+	scoring_instructions_div.html(scoring_instructions_html);
 }
 
 
@@ -89,119 +41,142 @@ function ajaxRequest(url, method, authToken, data, callback) {
 auth-token is genrated randomly with the prefix 'test-' as
 we need auth token for demo.*/
 function genrateAuthToken(){
-	var randomNo,
-		//variables for ajax request
-		url = apiBaseUrl + "/authenticate",
-		data = {},
-		headers = {},
-		method = "GET";
+	return new Promise(function(resolve, reject) {
+	  	var randomNo,
+			//variables for ajax request
+			url = apiBaseUrl + "/authenticate",
+			data = {},
+			method = "GET",
+			demoduration = '',
+			totalMinutes,
+			totalSeconds;
 
-	//random number generated between 1 to 10000 for auth-token
-	randomNo = Math.floor((Math.random() * 10000) + 1);
-	authToken = 'test-'+ randomNo;
-	console.log(authToken);
+		//random number generated between 1 to 10000 for auth-token
+		randomNo = Math.floor((Math.random() * 10000) + 1);
+		authToken = 'test-'+ randomNo;
 
-	//calling ajax request function
-	ajaxRequest(url, method, authToken, data,function (data) {
-		console.log(response);
+		//calling ajax request function
+		ajaxRequest(url, method, authToken, data,function (response) {
+			if(response != 'Invalid Token.'){
+				resolve(response);
+			}
+			else{
+				reject(alert("Error Occured"));
+			}
+		});
 	});
 }
+
 
 
 /*Function to display a question and options
  in question-div and answers div respectively.
  argument : index of element of  QuizQuestions array.*/
-function displayQuestion(index) {
-	var questionHtml= $("<h3>"),
+function displayQuestion() {
+	var //data for ajax request
+		url = apiBaseUrl + "/nextquestion?sid=" + sid,
+		method = "GET",
+		data = {},
+		//for displaing questions and answers
+		index,
+		questionHtml= $("<h3>"),
 		answerHtml= "",
 		questionDiv= $('#question-div'),
 		answerDiv= $('#answer-div'),
-		answerOptions;
+		answerOption;
 
-	//removing the html from divs
-	questionDiv.html('');
-	answerDiv.html('');
+	//calling ajax request function
+	ajaxRequest(url, method, authToken, data,function (data) {
+		//question index;
+		index = data.id;
 
-	//appending data attribute to question div
-	questionDiv.data('question-id', index);
+		//if there are questions
+		if(index !== "DEMOEND" ){
+			score = data.score;
+			//removing the html from divs
+			questionDiv.html('');
+			answerDiv.html('');
 
-	//appending question to questions-div
-	for(question in quizQuestions[index]){
-		questionHtml.html('<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>' + question);
-		answerOptions = quizQuestions[index][question];
+			//appending data attribute to question div
+			questionDiv.data('question-id', index);
 
-		//for answers
-		for(i in answerOptions){
-			//bootstrap grid system
-			answerHtml += '<div class="row">';
-			answerHtml += '<div class="col-sm-1"><input type="radio" name="option" value="'+i+'"></div>'
-			answerHtml += '<div class="col-sm-11"><pre><xmp style="padding:0px;margin:0px;white-space:nowrap">' + answerOptions[i] + '</xmp></pre><br></div>';
-			answerHtml += '</div>';
+			//appending question to questions-div
+			questionHtml.html('<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span> ' + data.str);
+			if(data.isMultiple){
+				questionHtml.append('<br><small>This is multiple choice question.</small>');
+			}
+			questionHtml.append('<br>');
+
+			//for answers
+			for(i in data.options){
+				answerOption = data.options[i];
+				//bootstrap grid system
+				answerHtml += '<div class="row">';
+				if(data.isMultiple){
+					answerHtml += '<div class="col-sm-1"><input type="checkbox" name="option" value="'+answerOption.id+'"></div>'				
+				}
+				else{
+					answerHtml += '<div class="col-sm-1"><input type="radio" name="option" value="'+answerOption.id+'"></div>'
+				}
+				answerHtml += '<div class="col-sm-11"><pre><xmp style="padding:0px;margin:0px;white-space:nowrap">' + answerOption.str + '</xmp></pre><br></div>';
+				answerHtml += '</div>';
+			}
+
+			questionDiv.append(questionHtml);
+			answerDiv.html(answerHtml);
+
+			//update scoring instructions
+			update_scoring_instructions(data.positive, data.negative);
+
+			//update score for question
+			update_score(score);
 		}
-	}
-	questionDiv.append(questionHtml);
-	answerDiv.html(answerHtml);
+		else{
+			window.location = 'report.html';
+		}
+	});
 }
 
 
 /*function to manage timer*/
 function show_timer(){
 	var timeLeftDiv = $("#time-left"),
-		minutes= 60,
-		seconds= 60,
-    	timer = minutes*seconds;
+		minutes,
+		seconds;
 
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+	setInterval(function () {
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        timeLeftDiv.html(minutes + ":" + seconds);
+		timeLeftDiv.html(minutes + ":" + seconds);
 
-        if (--timer < 0) {
-            window.location = 'report.html';
-        }
-    }, 1000);
+		if (--timer < 0) {
+		    window.location = 'report.html';
+		}
+	}, 1000);
 }
 
 
 /*function to update the score*/
-function update_score(questionIndex, answerValue){
+function update_score(score){
 	var last_score_div = $("#last-question-score"),
-		total_score_div = $("#total-score");
+		total_score_div = $("#total-score"),
+		previous_score = total_score_div.html();
 
-	if(correctAnswers[questionIndex] == answerValue){
-		score += 2.5;
-		$("#last-question-score").html(2.5);
-	}
-	else{
-		score += -3;
-		$("#last-question-score").html(-3);
-	}
-
+	last_score_div.html(score - previous_score);
 	total_score_div.html(score);
 }
 
 
 /* Function to initialize the gruclient*/
 function init(){
-	//instruction div will be hidden
+	//initially instruction div and quiz div will be hidden
 	$("#instructions-div").hide();
-
-	//make score divs zero
-	$("#total-score").html(0);
-	$("#last-question-score").html(0);
-
-	//it will check id quiz questions div is visible
-	if($('#quiz-questions').is(":visible")){
-		//call the displayQuestion function and passing the index of first QuizQuestions array
-		displayQuestion(0);
-
-		//start timer
-		show_timer();
-	}
+	$("#quiz-div").hide();
+	$("#timer-div").hide();
 }
 
 
@@ -230,82 +205,107 @@ function init(){
 })(jQuery, window);
 
 
-if($('#final-score').is(":visible")){
-	//write final score in report
-	$('#final-score').html(score);
-}
-
-
-/* If verify button is clicked */
+/* If start-demo button is clicked */
 $("#start-demo").on("click", function() {
-	//generating the auth token
-	genrateAuthToken();
+	genrateAuthToken().then(function (response) {
+		sid = response.id;
+		demoduration = response.demoduration;
+		//choosing the demodurationg string for total minutes and seconds
+		totalMinutes = (demoduration).substring(0,(demoduration).indexOf('m'));
+		totalSeconds = (demoduration).substring(demoduration.indexOf('m')+ 1,demoduration.indexOf('s'));
 
-	//remove the verification div
-	$("#demo-div").remove();
+		//convert the string into integer
+		totalMinutes = parseInt(totalMinutes);
+		totalSeconds = parseInt(totalSeconds);
+		timer = (totalMinutes*60) + totalSeconds;
 
-	//show the instructions div
-	$("#instructions-div").show();
+		//remove the verification div
+		$("#demo-div").remove();
+
+		//show the instructions div
+		$("#instructions-div").show();
+		});
+});
+
+
+/*if start test is clicked*/
+$("#start-test").on("click", function() {
+	$("#instructions-div").hide();
+	$("#quiz-div").show();
+	$("#timer-div").show();
+
+	//make score divs zero
+	$("#total-score").html(0);
+	$("#last-question-score").html(0);
+
+	//call the displayQuestion function and passing the index of first QuizQuestions array
+	displayQuestion();
+
+	//start timer
+	show_timer();
 });
 
 
 /* If skip question is clicked */
 $("#skip-question").on("click", function() {
-	var questionIndex;
+	var //variable for ajax request
+		url = apiBaseUrl + '/status',
+		method = "POST",
+		data={},
+		questionIndex,
 
 	//getting the current question id
 	questionIndex = $('#question-div').data().questionId;
 
-	//check if index is not of last question
-	if(questionIndex != quizQuestions.length -1){
-		displayQuestion(questionIndex + 1);
-		$("#last-question-score").html('');
-		$("#last-question-score").html(0);
-	}
-	else{
-		alert("Thier are no more questions.");
+	data['qid'] = questionIndex;
+	data['sid'] = sid;
 
-		//redirect to new page
-		window.location = 'report.html';
-	}
+	//ajax request
+	ajaxRequest(url, method, authToken, data,function (response) {
+		//call the function to display question
+		displayQuestion();
+	});
 });
 
 /* If skip question is clicked */
 $("#submit-answer").on("click", function() {
-	var questionIndex,
-		answerValue;
+	var //variable for ajax request
+		url = apiBaseUrl + '/status',
+		method = "POST",
+		data={},
+		questionIndex,
+		answerValue = [];
 
 	//getting the current question id
 	questionIndex = $('#question-div').data().questionId;
 
 	//getting the answer from question
-	answerValue = $('#answer-div').find('input[type="radio"]:checked').val();
+	$('input[name="option"]:checked').each(function() {
+		answerValue.push(this.value);
+    });
 
-	if(answerValue !== undefined){
-		//update score for answer;
-		update_score(questionIndex, answerValue);
+	//check if atleast an option is checked
+	if(answerValue.length !== 0){
+		data['qid'] = questionIndex;
+		data['sid'] = sid;
 
-		//check if index is not of last question
-		if(questionIndex != quizQuestions.length -1){
-			displayQuestion(questionIndex + 1);
+		for (var i = 0; i < answerValue.length; i++) {
+			data['aid'] = answerValue[i];
+			ajaxRequest(url, method, authToken, data,function (response) {
+			});
 		}
-		else{
-			alert("Test finished.");
-
-			//redirect to new page
-			window.location = 'report.html';
-		}
-
+		//call the function to display question
+		displayQuestion();
 	}
 	else{
 		alert("Please select an answer first.");
 	}
-
 });
 
 //if option text is clicked then also radio button should get selected
 $('#answer-div').on('click', 'pre', function() {
-	$(this).parent().parent().find('input[type="radio"]').prop('checked',true);
+	$(this).parent().parent().find('input[name="option"]').prop('checked',true);
 })
+
 
 init();
